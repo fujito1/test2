5ce2747e0970a0b0f329c78997f783121f12516d
fujit <bendam49@gmail.com>
2024-05-15 15:02:22 +0200

    server

---
+++ Server/src/de/porsche/Main.java
@@ -0,0 +1,31 @@
+    package de.porsche;
+
+    import java.io.IOException;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            for(int i=0;i< 3; i++){
+
+                int finalI = i;
+                Thread serverThread=new Thread(() ->{ //parameter of lambda expression is (), -> this operator separate the parameter from the body, {} the body
+                                                      //lambda expresion define behavior of each thread
+                    EchoServer server=new EchoServer();
+                    try {
+                        server.start(7788 + finalI);
+                        //server.stop();
+                    } catch (IOException e) {
+                        //System.err.println(e.getMessage());
+                        System.err.println("Error starting server on port" + (7788 + finalI) + ": " + e.getMessage());
+                    }
+                });
+                serverThread.start();
+
+                //why finalI, capturing the current value ensures that the lambda expression uses the specific
+                // value that was captured when it was created, even if the value of the original variable changes later on.
+
+            }
+
+
+        }
+    }
